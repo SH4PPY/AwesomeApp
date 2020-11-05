269e1a24ca3e38ff412244d79c08cbd6bcdb1bf9
James Shapcott <SH4PPY@users.noreply.github.com>
2020-11-05 11:14:44 +0000

    Initial commit

---
+++ AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            if (count == 69)
+            {
+                ((Button)sender).Text = $" {count} ...Heh...";
+            }
+            else
+            {
+                ((Button)sender).Text = $"You clicked {count} times.";
+            }
+        }
+    }
+}
